b5fb2ffbfec03bd2b6fc90a24dcd5679ba6b6da3
salman0325 <sk7587797@gmail.com>
2025-11-21 15:53:07 +0500

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -3,14 +3,19 @@
 //       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
 //   }
 // }
-def call(String SonarQubeAPI, String Projectname, String ProjectKey) {
+
+
+def call(String SonarQubeAPI, String ProjectName, String ProjectKey) {
 
     withSonarQubeEnv("${SonarQubeAPI}") {
 
+        // Locate Jenkins-installed SonarScanner tool
+        def scannerHome = tool 'SonarScanner'
+
         sh """
-            sonar-scanner \
+            ${scannerHome}/bin/sonar-scanner \
               -Dsonar.projectKey=${ProjectKey} \
-              -Dsonar.projectName=${Projectname} \
+              -Dsonar.projectName=${ProjectName} \
               -Dsonar.sources=. \
               -Dsonar.host.url=${SONAR_HOST_URL} \
               -Dsonar.login=${SONAR_AUTH_TOKEN}
