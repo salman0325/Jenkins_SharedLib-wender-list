84ed4ff061ea210591e1654f30f11449b37e5488
salman0325 <sk7587797@gmail.com>
2025-11-21 22:33:27 +0500

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,24 +1,24 @@
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
-}
-
-// def call(String projectName, String projectKey) {
-//     // Make sure the SonarScanner tool is configured in Jenkins
-//     def scannerHome = tool name: 'sonarqube1-token', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
-
-//     // Inject the SonarQube environment variables using the token
-//     withSonarQubeEnv('sonarqube1-token') {
-//         sh """
-//             ${scannerHome}/bin/sonar-scanner \
-//             -Dsonar.projectName=${projectName} \
-//             -Dsonar.projectKey=${projectKey} \
-//             -Dsonar.sources=. \
-//             -X
-//         """
-//     }
+// def call(String SonarQubeAPI, String Projectname, String ProjectKey){
+//   withSonarQubeEnv("${SonarQubeAPI}"){
+//       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+//   }
 // }
+
+def call(String projectName, String projectKey) {
+    // Make sure the SonarScanner tool is configured in Jenkins
+    def scannerHome = tool name: 'Sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+
+    // Inject the SonarQube environment variables using the token
+    withSonarQubeEnv('sonarqube1-token') {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectName=${ci-project} \
+            -Dsonar.projectKey=${project-ci} \
+            -Dsonar.sources=. \
+            -X
+        """
+    }
+}
 
 
 
