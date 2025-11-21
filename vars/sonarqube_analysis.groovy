93124645627e6a80a8142152d65b602938962e22
salman0325 <sk7587797@gmail.com>
2025-11-21 18:19:33 +0500

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -4,11 +4,19 @@
 //   }
 // }
 
-def call(String sonarServerName, String projectName, String projectKey) {
-    // withSonarQubeEnv injects the scanner path
-    withSonarQubeEnv(sonarServerName) {
-        echo "SONAR_SCANNER_HOME = ${env.SONAR_SCANNER_HOME}"
-        sh "${env.SONAR_SCANNER_HOME}/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectKey} -Dsonar.sources=. -X"
+def call(String projectName, String projectKey) {
+    // Make sure the SonarScanner tool is configured in Jenkins
+    def scannerHome = tool name: 'sonarqube1-token', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+
+    // Inject the SonarQube environment variables using the token
+    withSonarQubeEnv('sonarqube1-token') {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.sources=. \
+            -X
+        """
     }
 }
 
