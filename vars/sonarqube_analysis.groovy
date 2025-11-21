2f3f86e168c9a3d28eff83afde40cf7727e2a5a7
salman0325 <sk7587797@gmail.com>
2025-11-21 17:03:44 +0500

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,25 +1,25 @@
-// def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-//   withSonarQubeEnv("${SonarQubeAPI}"){
-//       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-//   }
-// }
-
-
-def call(String SonarQubeAPI, String ProjectName, String ProjectKey) {
-
-    withSonarQubeEnv("${SonarQubeAPI}") {
-
-        // Locate Jenkins-installed SonarScanner tool
-        def scannerHome = tool 'SonarScanner'
-
-        sh """
-            ${scannerHome}/bin/sonar-scanner \
-              -Dsonar.projectKey=${ProjectKey} \
-              -Dsonar.projectName=${ProjectName} \
-              -Dsonar.sources=. \
-              -Dsonar.host.url=${SONAR_HOST_URL} \
-              -Dsonar.login=${SONAR_AUTH_TOKEN}
-        """
-    }
+def call(String SonarQubeAPI, String Projectname, String ProjectKey){
+  withSonarQubeEnv("${SonarQubeAPI}"){
+      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
+  }
 }
+
+
+// def call(String SonarQubeAPI, String ProjectName, String ProjectKey) {
+
+//     withSonarQubeEnv("${SonarQubeAPI}") {
+
+//         // Locate Jenkins-installed SonarScanner tool
+//         def scannerHome = tool 'SonarScanner'
+
+//         sh """
+//             ${scannerHome}/bin/sonar-scanner \
+//               -Dsonar.projectKey=${ProjectKey} \
+//               -Dsonar.projectName=${ProjectName} \
+//               -Dsonar.sources=. \
+//               -Dsonar.host.url=${SONAR_HOST_URL} \
+//               -Dsonar.login=${SONAR_AUTH_TOKEN}
+//         """
+//     }
+// }
 
